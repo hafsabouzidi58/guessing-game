4075f4022dd6fa9bda35de08a6f686a7eaeddbe0
hafsabouzidi58 <bouzidihafsa75@gmail.com>
2024-11-27 03:53:26 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 
-# Fonction pour demander à l'utilisateur de deviner le nombre de fichiers
-function play_game {
-    local file_count=$(ls -1 | wc -l) # Compte le nombre de fichiers dans le répertoire actuel
-    local user_guess=-1 # Initialisation de la réponse de l'utilisateur
-    
-    echo "Bienvenue dans le jeu de devinettes !"
-    echo "Combien de fichiers se trouvent dans le répertoire actuel ? Essayez de deviner."
+# Obtenir le nombre de fichiers dans le répertoire actuel (en excluant les répertoires)
+correct_guess=$(ls -l | grep -v '^d' | wc -l)
 
-    # Boucle jusqu'à ce que l'utilisateur trouve le bon nombre
-    while [[ $user_guess -ne $file_count ]]; do
-        # Demande à l'utilisateur une estimation
-        read -p "Votre estimation : " user_guess
-        
-        # Vérifie si l'entrée est un nombre
-        if [[ ! $user_guess =~ ^[0-9]+$ ]]; then
-            echo "Veuillez entrer un nombre valide."
-            continue
-        fi
+# Boucle jusqu'à ce que l'utilisateur devine correctement
+while true; do
+    # Demander à l'utilisateur de deviner le nombre de fichiers
+    echo "Devinez le nombre de fichiers dans le répertoire actuel :"
+    read guess
 
-        # Comparaison de l'estimation avec le nombre réel de fichiers
-        if [[ $user_guess -lt $file_count ]]; then
-            echo "Trop bas ! Essayez encore."
-        elif [[ $user_guess -gt $file_count ]]; then
-            echo "Trop élevé ! Essayez encore."
-        else
-            echo "Félicitations ! Vous avez trouvé : il y a exactement $file_count fichiers."
-        fi
-    done
-}
-
-# Appel de la fonction
-play_game
+    # Vérifier si la réponse de l'utilisateur est trop basse, trop élevée ou correcte
+    if [[ $guess -lt $correct_guess ]]; then
+        echo "Votre estimation est trop basse."
+    elif [[ $guess -gt $correct_guess ]]; then
+        echo "Votre estimation est trop élevée."
+    else
+        echo "Bravo, vous avez trouvé le bon nombre de fichiers !"
+        break  # Quitter la boucle lorsque l'utilisateur devine correctement
+    fi
+done
